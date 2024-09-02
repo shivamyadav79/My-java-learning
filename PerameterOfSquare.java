9d974f59fdd7e68e53a13ca99c79266d450309df
shivam yadav <142260629+shivamyadav79@users.noreply.github.com>
2024-09-02 14:14:40 +0530

    Create PerameterOfSquare.java

---
+++ PerameterOfSquare.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class PerameterOfSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Area");
+        int a = sc.nextInt();
+        System.out.println("This is the Perameter " +a*a*a*a);
+    }
+}
